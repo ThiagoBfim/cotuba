2b637db37a985e4e9766f060ac873581c286c30d
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-07-29 21:01:48 -0300

    Cap 05 - Clean code

--- src/main/java/cotuba/application/GeradorEbookFactory.java
+++ src/main/java/cotuba/application/GeradorEbookFactory.java
@@ -1,18 +1,10 @@
 package cotuba.application;
 
 import cotuba.domain.Formato;
-import cotuba.gerador.GeradorEPUB;
-import cotuba.gerador.GeradorPDF;
 
 public class GeradorEbookFactory {
 
     public static GeradorEbook fabricar(Formato formato) {
-        if (formato == Formato.PDF) {
-            return new GeradorPDF();
-        } else if (formato == Formato.EPUB) {
-            return new GeradorEPUB();
-        } else {
-            throw new RuntimeException("Formato do ebook inválido: " + formato.name());
-        }
+        return formato.getGeradorEbook();
     }
 }
