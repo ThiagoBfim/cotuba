461f9b8c640059b10c9b87967a4fd79f9de07952
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-07-30 20:12:38 -0300

    Cap 07 - Criação do plugin

--- src/main/java/cotuba/plugin/Plugin.java
+++ src/main/java/cotuba/plugin/Plugin.java
@@ -1,6 +1,19 @@
 package cotuba.plugin;
 
+import java.util.List;
+import java.util.ServiceLoader;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 public interface Plugin {
 
     String cssDoTema();
+
+    static List<String> listaDeTemas() {
+        ServiceLoader<Plugin> loader = ServiceLoader.load(Plugin.class);
+        return StreamSupport.stream(loader.spliterator(), false)
+                .map(Plugin::cssDoTema)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/cotuba/tema/AplicadorTema.java
+++ src/main/java/cotuba/tema/AplicadorTema.java
@@ -3,14 +3,19 @@ package cotuba.tema;
 import com.itextpdf.html2pdf.jsoup.Jsoup;
 import com.itextpdf.html2pdf.jsoup.nodes.Document;
 import cotuba.domain.Capitulo;
+import cotuba.plugin.Plugin;
+
+import java.util.List;
 
 public class AplicadorTema {
 
     public void aplica(Capitulo capitulo) {
         String	html	=	capitulo.getConteudoHtml();
         Document document	=	Jsoup.parse(html);
-        String	css	=	"h1	{	border-bottom:	1px	dashed	black;	}";
-        document.select("head").append("<style>	"	+	css	+	"	</style>");
+        List<String> listaDeTemas	=	Plugin.listaDeTemas();
+        for	(String	css	:	listaDeTemas) {
+            document.select("head").append("<style>	" + css + "	</style>");
+        }
         capitulo.setConteudoHtml(document.html());
     }
 }
